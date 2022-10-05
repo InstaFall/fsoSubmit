a60c060421a4d48a5e54f7bfbdaa47c4fd014f60
InstaFall <gorkemcanakilli@hotmail.com>
2022-10-05 22:13:43 +0300

    Commit exercise 1.3

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -13,33 +13,38 @@ const Part = (props) => {
 const Content = (props) => {
 return(
 <div>
-<Part text={props.array[0].name} ex={props.array[0].exercises} />
-<Part text={props.array[1].name} ex={props.array[1].exercises} />
-<Part text={props.array[2].name} ex={props.array[2].exercises} />
+<Part text={props.p1.name} ex={props.p1.exercises} />
+<Part text={props.p2.name} ex={props.p2.exercises} />
+<Part text={props.p3.name} ex={props.p3.exercises} />
 </div>
 )
 }
 
 const Total = (props) => {
 return (
-  <p>Number of exercises {props.array[0].exercises + props.array[1].exercises + props.array[2].exercises}</p>
+  <p>Number of exercises {props.p1.exercises + props.p2.exercises + props.p3.exercises}</p>
 )
-
 }
-const App = () => {
-  const course = 'Half Stack application development'
-
-  const pArray = [
-    { name: 'Fundamentals of React', exercises: 10 },
-    { name: 'Using props to pass data', exercises: 7 },
-    { name: 'State of a component', exercises: 14 }
-  ]
+  const App = () => {
+    const course = 'Half Stack application development'
+    const part1 = {
+      name: 'Fundamentals of React',
+      exercises: 10
+    }
+    const part2 = {
+      name: 'Using props to pass data',
+      exercises: 7
+    }
+    const part3 = {
+      name: 'State of a component',
+      exercises: 14
+    }
 
   return (
     <div>
       <Header text={course}/>
-      <Content array={pArray}/>
-      <Total array={pArray}/>
+      <Content p1={part1} p2={part2} p3={part3}/>
+      <Total p1={part1} p2={part2} p3={part3}/>
     </div>
   )
 }
