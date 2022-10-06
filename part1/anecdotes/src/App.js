4898c148ff6faaa2e035f10385119f194e2f37bd
InstaFall <gorkemcanakilli@hotmail.com>
2022-10-06 23:05:46 +0300

    Commit exercise 1.12

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -10,12 +10,17 @@ const App = () => {
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.'
   ]
-   
+
   const [selected, setSelected] = useState(0)
+
+  const rand = (max) => {
+    return (Math.floor(Math.random() * max))
+  }
 
   return (
     <div>
-      {anecdotes[selected]}
+      {anecdotes[selected]}<br />
+      <button onClick={() => setSelected(rand(anecdotes.length))}>next anecdote</button>
     </div>
   )
 }
