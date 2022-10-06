b6aec167428c1724bf18c49fbb877490df4216ce
InstaFall <gorkemcanakilli@hotmail.com>
2022-10-06 21:37:22 +0300

    Commit exercise 1.6

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,5 +1,21 @@
 import { useState } from 'react'
 
+// Button component
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
+
+// Header component
+const Header = ({ h, text }) => {
+  switch (h) {
+    case `h1`: return (<h1>{text}</h1>)
+    case `h2`: return (<h2>{text}</h2>)
+    case `h3`: return (<h3>{text}</h3>)
+    case `h4`: return (<h4>{text}</h4>)
+    case `h5`: return (<h5>{text}</h5>)
+    case `h6`: return (<h6>{text}</h6>)
+    default: return (<h1>Title</h1>)
+  }
+
+}
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
@@ -8,7 +24,14 @@ const App = () => {
 
   return (
     <div>
-      code here
+      <Header h='h1' text='give feedback' />
+      <Button onClick={() => setGood(good + 1)} text='good' />
+      <Button onClick={() => setNeutral(neutral + 1)} text='neutral' />
+      <Button onClick={() => setBad(bad + 1)} text='bad' />
+      <Header h='h2' text='statistics' />
+      good {good}<br />
+      neutral {neutral}<br />
+      bad {bad}
     </div>
   )
 }
