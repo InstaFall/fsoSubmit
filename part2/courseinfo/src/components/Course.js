093f5ea663615ca36838691651d6d74d00c737ff
InstaFall <gorkemcanakilli@hotmail.com>
2022-10-07 21:25:13 +0300

    Commit exercise 2.1

---
+++ part2/courseinfo/src/components/Course.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Header = ({ course }) => <h1>{course}</h1>
+
+const Part = ({ part }) => <p>{part.name} {part.exercises}</p>
+
+const Content = ({ parts }) => <>{parts.map(el => <Part key={el.id} part={el} />)}</>
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+    </div>
+
+  )
+}
+
+export default Course
---
+++ part2/courseinfo/src/index.js
@@ -0,0 +1,7 @@
+// Using React 18 version of index.js | Full Stack Open 2022
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
