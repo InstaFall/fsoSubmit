9af6b858da16b682dbe7a96ea72ae8cdbde9b1ca
InstaFall <gorkemcanakilli@hotmail.com>
2022-10-07 21:37:26 +0300

    Commit exercise 2.2

--- part2/courseinfo/src/components/Course.js
+++ part2/courseinfo/src/components/Course.js
@@ -6,11 +6,14 @@ const Part = ({ part }) => <p>{part.name} {part.exercises}</p>
 
 const Content = ({ parts }) => <>{parts.map(el => <Part key={el.id} part={el} />)}</>
 
+const Sum = ({ parts }) => <p><b>Total of {parts.reduce((sum, el) => sum += el.exercises, 0)} exercises</b></p>
+
 const Course = ({ course }) => {
   return (
     <div>
       <Header course={course.name} />
       <Content parts={course.parts} />
+      <Sum parts={course.parts} />
     </div>
 
   )
