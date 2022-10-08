08389a8ae555723b92dedda5e1ff392a3a643e73
InstaFall <gorkemcanakilli@hotmail.com>
2022-10-08 23:13:38 +0300

    Commit exercise 2.8

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,12 +2,17 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-1234567' }
   ])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
-  const handleInput = (event) => {
+  const handleInputName = (event) => {
     setNewName(event.target.value)
+  }
+
+  const handleInputNumber = (event) => {
+    setNewNumber(event.target.value)
   }
 
   const handleSubmit = (event) => {
@@ -15,13 +20,17 @@ const App = () => {
     if (persons.some(e => e.name === newName)) {
       alert(`${newName} is already in the phonebook!`)
       setNewName('')
+      setNewNumber('')
       document.querySelector('input').focus()
     }
+
     else {
       const newPerson = {
-        name: newName
+        name: newName,
+        number: newNumber
       }
       setNewName('')
+      setNewNumber('')
       setPersons(persons.concat(newPerson))
     }
   }
@@ -31,7 +40,10 @@ const App = () => {
       <h2>Phonebook</h2>
       <form>
         <div>
-          name: <input type="text" value={newName} onChange={handleInput} />
+          name: <input type="text" value={newName} onChange={handleInputName} />
+        </div>
+        <div>
+          number: <input type="tel" value={newNumber} onChange={handleInputNumber} />
         </div>
         <div>
           <button type="submit" onClick={handleSubmit}>add</button>
@@ -39,7 +51,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map((el) => <li key={el.name}>{el.name}</li>)}
+        {persons.map((el) => <li key={el.name}>{el.name} {el.number}</li>)}
       </ul>
     </div>
   )
